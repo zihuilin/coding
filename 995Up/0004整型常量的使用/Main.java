f1b8f60efe8b90dff685a9c93741a6c8cef6f80b
Lin Zihui <zihui.lin@163.com>
2019-12-30 20:24:19 +0800

    0011

---
+++ 995Up/0004整型常量的使用/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        //long a = 12345678900L;
+        //System.out.println(a);
+        int a = 0x2A;
+        int b = a + 1000L;
+        System.out.println(b);
+    }
+}
