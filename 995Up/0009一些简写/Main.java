e71d164f7912f1e80e83fe9093bd5acfa68d5309
Lin Zihui <zihui.lin@163.com>
2020-01-02 16:10:10 +0800

    0013

---
+++ 995Up/0009一些简写/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+
+    public static void main(String[] args){
+        int a = 9;
+        System.out.println(a--);
+        a %= 3;
+        System.out.println(a);
+    }
+}
--- 995Up/0010数据类型的转换/Main.java
+++ 995Up/0010数据类型的转换/Main.java
@@ -1,6 +1,13 @@
 public class Main{
 
     public static void main(String[] args){
+        //long a = 10;
+        //int b = (int)(a + 10);
+        //int a =1234567893;
+        //double f = a * 0.1;
+        float f = 1234.56F;
+        int a = (int)f * 2;
+        System.out.println(a);
 
     }
 }
