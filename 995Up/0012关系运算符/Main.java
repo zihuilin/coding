2f004e598ca4b84361250d83058d11fc63debecb
Lin Zihui <zihui.lin@163.com>
2020-01-04 18:50:01 +0800

    0013

---
+++ 995Up/0012关系运算符/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+
+    public static void main(String[] args){
+        int a = 4;
+        float b = 10.5f;
+        System.out.println(a==4);
+    }
+}
---
+++ 995Up/0013if_else语句/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        int a = 40;
+        int b = 10;
+
+        //输出a和b中较小的一个
+        if (a<b){
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}
