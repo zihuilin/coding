f9f4aa4121b76c9a1c6087eff11a9c7bdbff4d9d
Lin Zihui <zihui.lin@163.com>
2020-05-22 16:56:26 +0800

    Week12 Fri

--- 998数据结构/19大数据2班/029List_As_Queue.py
+++ 998数据结构/19大数据2班/029List_As_Queue.py
@@ -31,13 +31,37 @@ class LQueue:
             self.tail = self.tail + 1
             self.list[self.tail] = data
 
+    def get(self):
+        #1. 空队列
+        #2. 只有一个数据: head,tail都为-1
+        #3. 普通情况: head向后移
+        #4. 特别情况：head移动到开头
+
     def print_all(self): #打印队列 (4)-(2)-(15)...(6)
-        print(self.list)
-        return None
+        if self.head == -1: #空队列
+            return
+        elif self.head <= self.tail: #head在tail前面
+            for i in range(self.head, self.tail+1):
+                print("(" + str(self.list[i]) + ")", end='')
+                if i!= self.tail:
+                    print("-", end='')
+        elif self.head > self.tail: #head在tail后面
+            for i in range(self.head, self.size):
+                print("(" + str(self.list[i]) + ")", end='')
+            for i in range(0, self.tail+1):
+                print("(" + str(self.list[i]) + ")", end='')
+                if i!= self.tail:
+                    print("-", end='')
+
+
 
 q = LQueue(6)
-q.put(4)
+q.put(1)
 q.put(2)
+q.put(3)
+q.put(4)
 q.put(5)
+q.put(6)
+q.put(7)
 q.print_all()  #(4)-(2)-(5)
 
